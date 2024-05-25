601e3851d32dc0857b9dd883bd734239f867f164
Ruben Ferreira <ruben_mpff@hotmail.com>
2024-05-25 21:22:49 +0100

    update app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginRegisterComponent } from './components/front-office/login-register/login-register.component';
+import { HomeComponent } from './components/front-office/home/home/home.component';
+import { ContactComponent } from './components/front-office/contact/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'login', component: LoginRegisterComponent },
+    { path: 'contato', component: ContactComponent },
+    { path: '**', redirectTo: '' }
+];
