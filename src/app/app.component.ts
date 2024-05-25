17ee7a129e54896282535fe327855b6ad9b0c65e
Ruben Ferreira <ruben_mpff@hotmail.com>
2024-05-25 21:20:13 +0100

    add app component html and ts and add app config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
+import { FooterComponent } from './components/shared/footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
